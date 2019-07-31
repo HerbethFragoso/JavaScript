80587c4fc2ce016a0d78392e95ca4a09a02108f8
HerbethFragoso <42307493+HerbethFragoso@users.noreply.github.com>
2019-07-30 23:12:35 -0300

    Add files via upload

---
+++ Aulas Js/TipagemFraca.js
@@ -0,0 +1,9 @@
+let qualquer = 'legal'
+console.log (qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.3566
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//evite nomes genericos para as suas variaveis.
---
+++ Aulas Js/templateString.js
@@ -0,0 +1,9 @@
+const nome = 'Rebeca'
+const concatenacao = 'Olá ' + nome + '!'
+
+console.log(concatenacao)
+
+console.log('1 + 1= ${1 + 1}')
+
+const up = texto.toUpperCasse()
+console.log('Ei.... ${up(cuoidado)} !')
